a22ba271ee24a948ade2d56ad6a760052656c42a
Simon Balean <simonbalean@gmail.com>
2018-11-27 09:27:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,8 +13,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-
+                 
+shipping_manifest[oil_paintings]
 end
 
 def adding
